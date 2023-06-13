fff2ce1b779f11f8035cbbf68f3f9cdeab6464f8
Tupalski Ilya <itupalski@gmail.com>
2023-06-13 10:39:19 +0400

    feat: add contacts page (Tue Jun 13 10:39:19 +04 2023)

--- js/modules/page-switching.js
+++ js/modules/page-switching.js
@@ -15,14 +15,14 @@ function animateSwitching(attribute) {
       photoRight.style.right = "0px";
     }
     if (section.classList.contains("info_active")) {
-      section.style.top = '-100%';
-      section.classList.remove('info_active');
+      section.style.top = "-100%";
+      section.classList.remove("info_active");
     }
-  })
+  });
   sectionMain.forEach((section) => {
     if (section.id === attribute) {
-      section.style.top = '50%';
-      section.classList.add('info_active');
+      section.style.top = "50%";
+      section.classList.add("info_active");
     }
   });
 }
@@ -34,7 +34,7 @@ function switchingPage() {
         if (activeLink.classList.contains("nav__link_active")) {
           activeLink.classList.remove("nav__link_active");
         }
-      })
+      });
       animateSwitching(clickLink.target.getAttribute("data-name"));
       clickLink.target.parentNode.classList.add("nav__link_active");
     });
